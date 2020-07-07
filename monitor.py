92b5fe37a88c0f04cc70012c302a1678b1c0c455
codeman-pi-projects <40361046+codeman-pi-projects@users.noreply.github.com>
2020-07-06 22:43:35 -0400

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -107,8 +107,8 @@ for key, pin in keysConfig.items('KEYS'):
 
 for key, pinSet in keysConfig.items('COMBOS'):
     pins = set(map(int, pinSet.split(',')))
-    KEY_COMBOS.update({frozenset(pins): getattr(uinput, key.upper())})
-    print(getattr(uinput, key.upper()))
+    KEY_COMBOS.update({frozenset(pins): KEY_ESC)#getattr(uinput, key.upper())})
+    print(getattr(uinput, KEY_ESC)
     
     
     
